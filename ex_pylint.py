9b4042934693ffcabb466945a419ccabd7f13dad
Luisa <luisaferreirass08@gmail.com>
2024-09-24 07:24:30 -0300

    pylint initials config

---
+++ ex_pylint.py
@@ -0,0 +1,10 @@
+print("Ola mundo")
+
+def minha_funcao():
+    """ As funções vão precisar ter o nome separado por _ e precisar 
+    de alguma documentação """
+    print("Estou na minha funcao")
+
+# snake_case -> como fazemos funções e variaveis em python usando o _
+# cammelCase -> Não consta
+# PascalCase -> como fazemos classes
