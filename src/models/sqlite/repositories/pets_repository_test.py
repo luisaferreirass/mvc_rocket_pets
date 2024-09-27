03cf89547e03739c62949bff7d99c0209c5362db
Luisa <luisaferreirass08@gmail.com>
2024-09-27 08:05:50 -0300

    finishing tests in pet repository

--- src/models/sqlite/repositories/pets_repository_test.py
+++ src/models/sqlite/repositories/pets_repository_test.py
@@ -1,11 +1,13 @@
 from unittest import mock
+import pytest
 from mock_alchemy.mocking import UnifiedAlchemyMagicMock
+from sqlalchemy.orm.exc import NoResultFound
 from src.models.sqlite.entities.pets import PetsTable
 from .pets_repository import PetsRepository
 
 # A gente vai usar o mock alchemy como gerente do banco de dados
 
-class MockConnection:
+class MockConnection: # Essa classe não possui erro
     def __init__(self) -> None:
         self.session = UnifiedAlchemyMagicMock(
             data= [
@@ -35,3 +37,51 @@ def test_list_pets():
     mock_connection.session.filter.assert_not_called()
 
     assert response[0].name == "dog"
+
+
+def test_delete_pets():
+    mock_connection = MockConnection()
+    repo = PetsRepository(mock_connection)
+
+    repo.delete_pets("petName")
+
+    mock_connection.session.query.assert_called_once_with(PetsTable)
+    mock_connection.session.filter.assert_called_once_with(PetsTable.name == "petName")
+    mock_connection.session.delete.assert_called_once()
+
+class MockConnectionNoResult: # Essa classe possui erro
+    def __init__(self) -> None:
+        self.session = UnifiedAlchemyMagicMock()
+        self.session.query.side_effect = self.__raise_no_result_found
+        # É o side effect que lança para gente a função do nosso erro
+        # que vai ser executado assim que a gente executar o query
+
+    def __raise_no_result_found(self, *args, **kwargs): 
+        # Todas as propriedades enviadas para la para cima que forem recebidas aqui
+        raise NoResultFound("No result found")
+
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+def test_list_pets_no_result():
+    mock_connection = MockConnectionNoResult()
+    repo = PetsRepository(mock_connection)
+    response = repo.list_pets()
+
+    mock_connection.session.query.assert_called_once_with(PetsTable)
+    mock_connection.session.all.assert_not_called()
+    mock_connection.session.filter.assert_not_called()
+
+    assert response == []
+
+def test_delete_pets_error():
+    mock_connection = MockConnectionNoResult()
+    repo = PetsRepository(mock_connection)
+
+    with pytest.raises(Exception): # Estamos conferindo mesmo se foi lançado o erro
+        repo.delete_pets("petName")
+
+    mock_connection.session.rollback.assert_called_once()
