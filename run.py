263e771fde431d34a158ca6186ff9f511af5cd57
Luisa <luisaferreirass08@gmail.com>
2024-10-07 12:08:03 -0300

    config: adding db to implementing deploy

--- run.py
+++ run.py
@@ -3,6 +3,6 @@
 from src.main.server.server import app
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=3000)
 # o host vai ser a minha própria máquina
 # Em ambientes de produção usamos o debug como false
