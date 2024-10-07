3eeba3112f5be42f2c3a5929f6627a680e07d391
Luisa <luisaferreirass08@gmail.com>
2024-10-07 07:22:00 -0300

    feat: implemeneting project routes for pets and people

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -5,6 +5,7 @@ from src.models.sqlite.settings.connection import db_connection_handler
 
 # Importar blueprints
 from src.main.routes.pets_route import pet_route_bp
+from src.main.routes.person_routes import person_route_bp
 
 db_connection_handler.connect_to_db() 
 # Primeiro estamos nos conectando ao banco de dados depois estamos 
@@ -14,5 +15,6 @@ app = Flask(__name__)
 CORS(app) # ?
 
 app.register_blueprint(pet_route_bp)
+app.register_blueprint(person_route_bp)
 # Todas as rotas que colocamos nesse bluprint estão
 # cadastradas no nosso app
